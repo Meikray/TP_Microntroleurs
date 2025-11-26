e5d1ab3a23b6bd652e29061d54c1fe255586c3d3
Meikray <f5ch5jppfh@privaterelay.appleid.com>
2025-11-26 11:43:39 +0100

    tp2.2

--- Ressources/TP_Microncontroleurs/TP2/TP2b_timer2_scrutation.X/main.s
+++ Ressources/TP_Microncontroleurs/TP2/TP2b_timer2_scrutation.X/main.s
@@ -7,7 +7,8 @@ config RSTOSC = HFINTOSC_64MHZ ; Horloge interne de 64 MHz
 config WDTE = OFF              ; Desactiver le watchdog    timer
     
 PSECT   code, abs
-   
+ ;Après configuration du Timer 2 et test à l?oscilloscope sur la  LED_MATRIX,
+; j?ai mesuré une période de 20 microsecondes, ce qui correspond bien au résultat attendu.
 ; Table des I/O ================================================================
 ; LED LED0 à la pin RC0
    
@@ -40,29 +41,29 @@ init_2:
     movwf   T2CON        
 
     BANKSEL T2CLKCON
-    movlw   0b00000001   ; Choisit la source du Timer2 : Fosc/4
+    movlw   0b00000001   ;Choisit la source du Timer2 : Fosc/4
     movwf   T2CLKCON     
 
     BANKSEL PIR4
-    bcf     PIR4, 1      ; Efface le flag TMR2IF (évènement Timer2)
+    bcf     PIR4, 1      ;Efface le flag TMR2IF (évènement Timer2)
 
-    goto loop            ; Va voir la boucle principale
+    goto loop            ;Va voir la boucle principale
 
  
 main:
     
 loop:
 
-    BANKSEL PIR4         ; Accède au registre PIR4
+    BANKSEL PIR4         ;Accède à PIR4
     btfss   PIR4,1       
-    goto loop            ; recommence la boucle si la boucle ne marche pas
+    goto loop            ;RecommenceR la boucle si la boucle ne marche pas
 
-    bcf     PIR4,1       ;effacer le flag TMR2IFsi la boucle marche
+    bcf     PIR4,1       ;Effacer le flag TMR2IFsi la boucle marche
 
     BANKSEL LATB         
     btg     LATB,4       
 
-    goto main            ; Retourne au début du programme
+    goto main            ;Retourne au début du programme
 
 end
 
